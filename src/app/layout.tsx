4c52f6e1662ea4667cbeacf5706acbf0dc071fe0
Davi Cheiz <davicheizz9@gmail.com>
2026-01-18 00:39:32 +0000

    novamente nao ta funcionando e ta dando essa mensagem, porra cara, como

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,18 +20,20 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@700&display=swap" rel="stylesheet" />
-        <Script id="utmify-pixel-id" strategy="beforeInteractive">
-          {`window.pixelId = "696be8829114906f7c23c18d";`}
-        </Script>
-        <Script 
-          id="utmify-pixel-src"
-          src="https://cdn.utmify.com.br/scripts/pixel/pixel.js"
-          strategy="afterInteractive"
-        />
       </head>
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <Script id="utmify-pixel" strategy="afterInteractive">
+          {`
+            window.pixelId = "696be8829114906f7c23c18d";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
+          `}
+        </Script>
       </body>
     </html>
   );
